004ea1316d7aa9dec430cd692e498a235967b17a
laijinwei <18776666967@163.com>
2023-09-24 14:44:49 +0800

    feature: configure django database

--- meiduo_mall/settings.py
+++ meiduo_mall/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'meiduo_mall.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'vm1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'NAME': 'meiduo_mall',
     }
 }
 
