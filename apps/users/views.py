1691cce51f377393bc67b9b10366c6193f48b673
laijinwei <18776666967@163.com>
2023-10-09 22:13:13 +0800

    feature: 优化注册环节代码

--- apps/users/views.py
+++ apps/users/views.py
@@ -2,6 +2,7 @@ import json
 import logging
 import re
 
+from django.contrib.auth import login
 from django.http import JsonResponse
 
 # Create your views here.
@@ -26,8 +27,7 @@ class MobileCount(View):
 
 class RegisterView(View):
     def post(self, request):
-        json_str = request.body
-        info_dict = json.loads(json_str)
+        info_dict = json.loads(request.body)
 
         username = info_dict.get("username")
         password = info_dict.get("password")
@@ -40,14 +40,28 @@ class RegisterView(View):
             return JsonResponse({'code': 400, 'errmsg': '参数不全'})
 
         # 验证用户名是否符合规则，是否重复
-        if not re.match(r'[\da-zA-Z_]{5,20}', username) or User.objects.filter(username=username).count():
-            return JsonResponse({'code': 400, 'errmsg': '用户名不符合规则'})
+        count = User.objects.filter(username=username).count()
+        if not re.match(r'[\da-zA-Z_]{5,20}', username):
+            return JsonResponse({'code': 400, 'count': count, 'errmsg': '用户名不符合规则'})
+
+        if count != 0:
+            return JsonResponse({'code': 400, 'count': count, 'errmsg': '用户名已存在！'})
 
         if password != password2:
             return JsonResponse({'code': 400, 'errmsg': '密码不一致'})
-        if not re.match(r'[\d]{11}$', mobile) or User.objects.filter(phone=mobile).count():
-            return JsonResponse({'code': 400, 'errmsg': '手机号码不符合规则'})
 
-        User.objects.create_user(username=username, password=password, phone=mobile)
+        count = User.objects.filter(phone=mobile).count()
+        if not re.match(r'[\d]{11}$', mobile):
+            return JsonResponse({'code': 400, 'count': count, 'errmsg': '手机号码不符合规则'})
 
-        return JsonResponse({'code': 0, 'errmsg': 'ok'})
+        if count != 0:
+            return JsonResponse({'code': 400, 'count': count, 'errmsg': '手机号码已注册！'})
+
+        user = User.objects.create_user(username=username, password=password, phone=mobile)
+
+        login(request, user)
+
+        res = JsonResponse({'code': 0, 'errmsg': 'ok'})
+        res.set_cookie("username", username)
+
+        return res
